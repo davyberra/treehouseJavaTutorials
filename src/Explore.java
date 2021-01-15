205626269e106228d6de18b40e46e3128e62f5cd
unknown <davyberra@gmail.com>
2021-01-15 14:51:15 -0800

    Finished 2 of 3 Array lessons

--- src/Explore.java
+++ src/Explore.java
@@ -1,6 +1,20 @@
 public class Explore {
 
     public static void main(String[] args) {
+        String[] friends = {"Ben", "Alena", "Pasan"};
+        // Enhanced for loop
+        for (String friend : friends) {
+            System.out.printf("Hey %s! The movie starts at 7, see you there! %n",
+                    friend);
+        }
+
+        // Regular for loop
+        for (int i = 0; i < friends.length; i++) {
+            String friend = friends[i];
+            System.out.printf("Hey %s! The movie starts at 7, see you there! %n",
+                    friend);
+        }
+
 
     }
 
