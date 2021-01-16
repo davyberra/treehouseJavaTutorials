be47375d9deaa5a3c46190e0e6e4278719e47e8c
unknown <davyberra@gmail.com>
2021-01-15 21:00:09 -0800

    Scratch practice

--- src/scratch.java
+++ src/scratch.java
@@ -1,4 +1,20 @@
-int[][] scoreCards = {
-        {1, 2, 3},
-        {2, 3, 5},
-        };
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Random;
+
+String[] friends = {
+        "Nick",
+        "Liz",
+        "Brittany",
+        "Davy" };
+
+
+public String pickLunchSpot(String... spots) {
+    System.out.printf("Randomly picking from %d lunch spots. %n",
+        spots.length);
+    if (spots.length = 0) {
+        return "Someplace with tacos";
+        }
+    Random random = new Random();
+    return spots[random.nextInt(spots.length)];
+    }
